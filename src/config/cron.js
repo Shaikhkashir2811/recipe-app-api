304ae29d0c21f4cb7a08e5a5645f3b3b3b65ea31
Shaikh Kashir Mahammed Aarif <shaikhkashir2876@gmail.com>
2026-03-17 16:55:23 +0530

    cron

--- src/config/cron.js
+++ src/config/cron.js
@@ -1,11 +1,13 @@
-import cron from "cron";
+import { CronJob } from "cron";
 import https from "https";
 
-const job = new cron.job("*/14 * * * *",function(){
-    https.get(process.env.API_URL, (res)=>{
-        if(res.statusCode === 200) console.log("Get request sent successfully ");
-        else console.log("Failed to send get request ",res.statusCode);
-    }).on("error",(e)=> console.error("Error sending get request ",e));
+const job = new CronJob("*/14 * * * *", function () {
+    https.get(process.env.API_URL, (res) => {
+        if (res.statusCode === 200)
+            console.log("Get request sent successfully");
+        else
+            console.log("Failed to send get request", res.statusCode);
+    }).on("error", (e) => console.error("Error sending get request", e));
 });
 
-export default job; 
+export default job;
